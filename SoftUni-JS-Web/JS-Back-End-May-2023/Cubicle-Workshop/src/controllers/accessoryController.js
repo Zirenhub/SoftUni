1999c9ae7f60be704557373dc1858ed1f927c095
Erdinch <erdinchorhan02@gmail.com>
2023-05-30 18:12:38 +0300

    get and post for attach accessory

--- SoftUni-JS-Web/JS-Back-End-May-2023/Cubicle-Workshop/src/controllers/accessoryController.js
+++ SoftUni-JS-Web/JS-Back-End-May-2023/Cubicle-Workshop/src/controllers/accessoryController.js
@@ -1,4 +1,6 @@
 const AccessoryModel = require('../models/Accessory');
+const CubeModel = require('../models/Cube');
+const mongoose = require('mongoose');
 
 const createAccessory = async (req, res) => {
   const { name, description, imageUrl } = req.body;
@@ -10,6 +12,32 @@ const createAccessory = async (req, res) => {
   res.redirect('/');
 };
 
+const getAttachAccessory = async (req, res) => {
+  const cubeId = req.params.id;
+  const cube = await CubeModel.findById(cubeId).lean();
+  const accessories = await AccessoryModel.find({
+    _id: { $nin: cube.accessories },
+  }).lean();
+
+  const hasAccessories = accessories.length > 0;
+
+  res.render('accessory/attach', { cube, accessories, hasAccessories });
+};
+
+const postAttachAccessory = async (req, res) => {
+  const { accessory } = req.body;
+  const cubeId = req.params.id;
+  // validate that the accessory isnt attach in the first place.
+  const accessoryObjectId = new mongoose.Types.ObjectId(accessory.trim());
+  await CubeModel.findByIdAndUpdate(cubeId, {
+    $push: { accessories: accessoryObjectId },
+  });
+
+  res.redirect(`/cube/details/${cubeId}`);
+};
+
 module.exports = {
+  getAttachAccessory,
+  postAttachAccessory,
   createAccessory,
 };
--- SoftUni-JS-Web/JS-Back-End-May-2023/Cubicle-Workshop/src/routes/accessory.js
+++ SoftUni-JS-Web/JS-Back-End-May-2023/Cubicle-Workshop/src/routes/accessory.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const { createAccessory } = require('../controllers/accessoryController');
+const {
+  createAccessory,
+  getAttachAccessory,
+  postAttachAccessory,
+} = require('../controllers/accessoryController');
 const router = express.Router();
 
 router.get('/create', (req, res) => {
   res.render('accessory/create');
 });
 router.post('/create', createAccessory);
+
+router.get('/attach/:id', getAttachAccessory);
+router.post('/attach/:id', postAttachAccessory);
 
 module.exports = router;
