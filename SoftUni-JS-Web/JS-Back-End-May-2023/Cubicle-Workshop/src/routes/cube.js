1f5e3c42ab7b935e8daba1443dfb6ec27490f59c
Erdinch <erdinchorhan02@gmail.com>
2023-06-07 16:27:24 +0300

    routes

--- SoftUni-JS-Web/JS-Back-End-May-2023/Cubicle-Workshop/src/routes/cube.js
+++ SoftUni-JS-Web/JS-Back-End-May-2023/Cubicle-Workshop/src/routes/cube.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const { createCube, detailsCube } = require('../controllers/cubeController');
+const {
+  createCube,
+  detailsCube,
+  getEditCube,
+  postEditCube,
+  getDeleteCube,
+  postDeleteCube,
+} = require('../controllers/cubeController');
 const { isAuth } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.get('/create', isAuth, (req, res) => {
   res.render('cube/create');
 });
-router.post('/create', isAuth, createCube);
-
 router.get('/details/:id', detailsCube);
+router.get('/edit/:id', isAuth, getEditCube);
+router.get('/delete/:id', isAuth, getDeleteCube);
+
+router.post('/create', isAuth, createCube);
+router.post('/edit/:id', isAuth, postEditCube);
+router.post('/delete/:id', isAuth, postDeleteCube);
 
 module.exports = router;
