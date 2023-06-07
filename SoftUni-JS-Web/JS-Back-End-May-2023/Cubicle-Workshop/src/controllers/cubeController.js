f19eb5999a2a2d3adafdbb819a4a80725abb15f1
Erdinch <erdinchorhan02@gmail.com>
2023-06-07 16:25:58 +0300

    edit, delete cube

--- SoftUni-JS-Web/JS-Back-End-May-2023/Cubicle-Workshop/src/controllers/cubeController.js
+++ SoftUni-JS-Web/JS-Back-End-May-2023/Cubicle-Workshop/src/controllers/cubeController.js
@@ -1,5 +1,6 @@
 const CubeModel = require('../models/Cube');
-const AccessoryModel = require('../models/Accessory');
+const createCubeOptions = require('../utils/createCubeOptions');
+const getCube = require('../utils/getCube');
 
 const getCubes = async (req, res) => {
   try {
@@ -21,19 +22,24 @@ const getCubes = async (req, res) => {
     res.render('index', { cubes, query: { search, from, to } });
   } catch (err) {
     console.log(err);
-    res.send(err.message);
+    next(err);
   }
 };
 
-const detailsCube = async (req, res) => {
-  const cube = await CubeModel.findById(req.params.id)
-    .populate('accessories')
-    .lean();
-  const hasAccessories = cube.accessories.length > 0;
-  if (cube) {
+const detailsCube = async (req, res, next) => {
+  try {
+    const cube = await CubeModel.findById(req.params.id)
+      .populate('accessories')
+      .lean();
+    if (!cube) {
+      throw new Error('Cube not found');
+    }
+    cube.isOwner = cube.owner.toString() === req.user._id;
+    const hasAccessories = cube.accessories.length > 0;
     res.render('cube/details', { cube, hasAccessories });
-  } else {
-    next();
+  } catch (err) {
+    console.log(err);
+    next(err);
   }
 };
 
@@ -41,20 +47,73 @@ const createCube = async (req, res) => {
   try {
     const { name, description, imageUrl, difficultyLevel } = req.body;
     if (!name || !description || !imageUrl || !difficultyLevel) {
-      res.end('All fields should be present');
-      // or "next();" for a 404 page.
+      throw new Error('All fields should be present');
     }
     const newCube = new CubeModel({
       name,
       description,
       imageUrl,
       difficultyLevel,
+      owner: req.user._id,
     });
     await newCube.save();
     res.redirect('/');
   } catch (err) {
     console.log(err);
-    res.send(err.message);
+    next(err);
+  }
+};
+
+const getEditCube = async (req, res, next) => {
+  try {
+    const cube = await getCube(req);
+    const options = createCubeOptions(cube.difficultyLevel);
+    res.render('cube/edit', { cube, options });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const postEditCube = async (req, res, next) => {
+  try {
+    const cube = await getCube(req);
+    const { name, description, imageUrl, difficultyLevel } = req.body;
+    if (!name || !description || !imageUrl || !difficultyLevel) {
+      throw new Error('All fields should be filled.');
+    }
+    cube.name = name;
+    cube.description = description;
+    cube.imageUrl = imageUrl;
+    cube.difficultyLevel = difficultyLevel;
+
+    await cube.save();
+
+    res.redirect(`/cube/details/${req.params.id}`);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const getDeleteCube = async (req, res, next) => {
+  try {
+    const cube = await getCube(req);
+    const options = createCubeOptions(cube.difficultyLevel);
+    res.render('cube/delete', { cube, options });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const postDeleteCube = async (req, res, next) => {
+  try {
+    await CubeModel.findByIdAndDelete(req.params.id);
+    res.redirect('/');
+  } catch (err) {
+    console.log(err);
+    next(err);
   }
 };
 
@@ -62,4 +121,8 @@ module.exports = {
   getCubes,
   createCube,
   detailsCube,
+  getEditCube,
+  postEditCube,
+  postDeleteCube,
+  getDeleteCube,
 };
