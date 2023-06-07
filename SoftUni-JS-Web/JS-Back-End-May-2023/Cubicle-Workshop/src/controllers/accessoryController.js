e7c2fb2b41343ffbfde213b57e85b38dd1dbf153
Erdinch <erdinchorhan02@gmail.com>
2023-06-07 16:25:36 +0300

    error handling

--- SoftUni-JS-Web/JS-Back-End-May-2023/Cubicle-Workshop/src/controllers/accessoryController.js
+++ SoftUni-JS-Web/JS-Back-End-May-2023/Cubicle-Workshop/src/controllers/accessoryController.js
@@ -1,54 +1,50 @@
+const mongoose = require('mongoose');
 const AccessoryModel = require('../models/Accessory');
 const CubeModel = require('../models/Cube');
-const mongoose = require('mongoose');
+const getCube = require('../utils/getCube');
 
-const createAccessory = async (req, res) => {
+const createAccessory = async (req, res, next) => {
   try {
     const { name, description, imageUrl } = req.body;
     if (!name || !description || !imageUrl) {
-      res.end('All fields should be filled.');
-    } else {
-      const newAccessory = new AccessoryModel({ name, description, imageUrl });
-      await newAccessory.save();
-      res.redirect('/');
+      throw new Error('All fields should be filled.');
     }
+    const newAccessory = new AccessoryModel({ name, description, imageUrl });
+    await newAccessory.save();
+    res.redirect('/');
   } catch (err) {
     console.log(err);
-    res.send(err.message);
+    next(err);
   }
 };
 
-const getAttachAccessory = async (req, res) => {
+const getAttachAccessory = async (req, res, next) => {
   try {
-    const cubeId = req.params.id;
-    const cube = await CubeModel.findById(cubeId).lean();
+    const cube = await getCube(req);
     const accessories = await AccessoryModel.find({
       _id: { $nin: cube.accessories },
     }).lean();
-
     const hasAccessories = accessories.length > 0;
 
     res.render('accessory/attach', { cube, accessories, hasAccessories });
   } catch (err) {
     console.log(err);
-    res.send(err.message);
+    next(err);
   }
 };
 
-const postAttachAccessory = async (req, res) => {
+const postAttachAccessory = async (req, res, next) => {
   try {
     const { accessory } = req.body;
-    const cubeId = req.params.id;
+    const cube = await getCube(req);
     // validate that the accessory isnt attach in the first place.
     const accessoryObjectId = new mongoose.Types.ObjectId(accessory.trim());
-    await CubeModel.findByIdAndUpdate(cubeId, {
-      $push: { accessories: accessoryObjectId },
-    });
-
+    cube.accessories.push(accessoryObjectId);
+    await cube.save();
     res.redirect(`/cube/details/${cubeId}`);
   } catch (err) {
     console.log(err);
-    res.send(err.message);
+    next(err);
   }
 };
 
