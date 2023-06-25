f13b0150cb6bc33943654ac7ded492b382105c09
Erdinch <erdinchorhan02@gmail.com>
2023-06-25 22:42:45 +0300

    list component

--- SoftUni-JS-Web/Angular-June-2023/todo/src/app/app.module.ts
+++ SoftUni-JS-Web/Angular-June-2023/todo/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent],
+  declarations: [AppComponent, CardComponent, ListComponent],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
