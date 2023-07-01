bd6fb9f543deeee9afcd9e9990b9ece8b0a84af3
Erdinch <erdinchorhan02@gmail.com>
2023-07-01 20:50:42 +0300

    get posts and dispaly them

--- SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/main/recent-posts/recent-posts.component.ts
+++ SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/main/recent-posts/recent-posts.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { Post } from 'src/types/post';
 
 @Component({
   selector: 'app-recent-posts',
   templateUrl: './recent-posts.component.html',
-  styleUrls: ['./recent-posts.component.css']
+  styleUrls: ['./recent-posts.component.css'],
 })
-export class RecentPostsComponent {
+export class RecentPostsComponent implements OnInit {
+  constructor(private api: ApiService) {}
 
+  posts: Post[] = [];
+
+  ngOnInit(): void {
+    this.api.getPosts(5).subscribe({
+      next: (posts) => {
+        console.log(posts);
+        this.posts = posts;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
