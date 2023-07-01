55eb420aee5bdfa4fbe3c29ce2e85ac0864af451
Erdinch <erdinchorhan02@gmail.com>
2023-07-01 20:50:51 +0300

    get themes and display them

--- SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/main/themes-list/themes-list.component.ts
+++ SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/main/themes-list/themes-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { Theme } from 'src/types/theme';
 
 @Component({
   selector: 'app-themes-list',
   templateUrl: './themes-list.component.html',
-  styleUrls: ['./themes-list.component.css']
+  styleUrls: ['./themes-list.component.css'],
 })
-export class ThemesListComponent {
+export class ThemesListComponent implements OnInit {
+  constructor(private api: ApiService) {}
 
+  themes: Theme[] = [];
+
+  ngOnInit(): void {
+    this.api.getThemes().subscribe({
+      next: (themes) => {
+        const themesSortedBySubs = themes.sort(
+          (a, b) => b.subscribers.length - a.subscribers.length
+        );
+        this.themes = themesSortedBySubs;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
