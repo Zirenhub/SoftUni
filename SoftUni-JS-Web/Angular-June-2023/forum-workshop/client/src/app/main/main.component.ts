f949ebb8122cdd76fe00b1ab75b872513a7c24d8
Erdinch <erdinchorhan02@gmail.com>
2023-07-08 21:45:49 +0300

    show welcome message if not logged in

--- SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/main/main.component.ts
+++ SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/main/main.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { UserService } from '../auth/user.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent {
+  constructor(private userService: UserService) {}
 
+  get isLoggedIn() {
+    return this.userService.isLogged;
+  }
 }
