1e3c2c41cb6416736e4644154fd1c90b098de2d7
Erdinch <erdinchorhan02@gmail.com>
2023-07-08 21:44:28 +0300

    app module decs/imports

--- SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/app.module.ts
+++ SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/app.module.ts
@@ -9,6 +9,11 @@ import { ThemesListComponent } from './main/themes-list/themes-list.component';
 import { RecentPostsComponent } from './main/recent-posts/recent-posts.component';
 import { ApiService } from './api.service';
 import { HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AuthModule } from './auth/auth.module';
+import { ThemeModule } from './theme/theme.module';
+import { UserService } from './auth/user.service';
+import { AuthActivate } from './core/guards/auth.activate';
 
 @NgModule({
   declarations: [
@@ -16,9 +21,17 @@ import { HttpClientModule } from '@angular/common/http';
     MainComponent,
     ThemesListComponent,
     RecentPostsComponent,
+    WelcomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
-  providers: [ApiService],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    HttpClientModule,
+    AuthModule,
+    ThemeModule,
+  ],
+  providers: [ApiService, UserService, AuthActivate],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
