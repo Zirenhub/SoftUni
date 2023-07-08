1631541e54f651bf3d09f95ebb9489d85b5ea68f
Erdinch <erdinchorhan02@gmail.com>
2023-07-08 21:47:01 +0300

    welcome component

---
+++ SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/welcome/welcome.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { UserService } from '../auth/user.service';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css'],
+})
+export class WelcomeComponent {
+  constructor(private userService: UserService) {}
+
+  get isLoggedIn() {
+    return this.userService.isLogged;
+  }
+}
