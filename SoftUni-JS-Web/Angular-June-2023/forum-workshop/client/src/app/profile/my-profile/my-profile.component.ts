7587f9a643d09589cac3b7cb3f8799c3ddd5ea88
Erdinch <erdinchorhan02@gmail.com>
2023-07-22 21:37:55 +0300

    edit profile page

--- SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/profile/my-profile/my-profile.component.ts
+++ SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/profile/my-profile/my-profile.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { EMAIL_DOMAINS } from 'src/app/shared/constants';
+import { emailValidator } from 'src/app/shared/validators/email-validator';
 
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
-  styleUrls: ['./my-profile.component.css']
+  styleUrls: ['./my-profile.component.css'],
 })
 export class MyProfileComponent {
+  isEditPage: boolean = false;
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        emailValidator(EMAIL_DOMAINS),
+      ],
+    ],
+    tel: [''],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  toggleEdit() {
+    this.isEditPage = !this.isEditPage;
+  }
+
+  handleSave() {
+    this.toggleEdit();
+    // todo
+  }
 }
--- SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/profile/profile.module.ts
+++ SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/profile/profile.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MyProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule],
+  imports: [CommonModule, ProfileRoutingModule, ReactiveFormsModule],
 })
 export class ProfileModule {}
