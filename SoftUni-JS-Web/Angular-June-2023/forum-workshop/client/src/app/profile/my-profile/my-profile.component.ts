60ce40e23947525a0dcd502b0471c1b36145524c
Erdinch <erdinchorhan02@gmail.com>
2023-07-25 17:25:53 +0300

    display actual profile info, update profile

--- SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/profile/my-profile/my-profile.component.ts
+++ SoftUni-JS-Web/Angular-June-2023/forum-workshop/client/src/app/profile/my-profile/my-profile.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { UserService } from 'src/app/auth/user.service';
 import { EMAIL_DOMAINS } from 'src/app/shared/constants';
 import { emailValidator } from 'src/app/shared/validators/email-validator';
+import { Profile } from 'src/types/profile';
 
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
   styleUrls: ['./my-profile.component.css'],
 })
-export class MyProfileComponent {
+export class MyProfileComponent implements OnInit {
   isEditPage: boolean = false;
   form = this.fb.group({
     username: ['', [Validators.required, Validators.minLength(5)]],
@@ -23,14 +25,26 @@ export class MyProfileComponent {
     tel: [''],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private userService: UserService) {}
+
+  ngOnInit(): void {
+    if (this.userService.user) {
+      const { username, email, tel } = this.userService.user;
+      this.form.setValue({ username, email, tel });
+    }
+  }
 
   toggleEdit() {
     this.isEditPage = !this.isEditPage;
   }
 
   handleSave() {
-    this.toggleEdit();
-    // todo
+    if (this.form.invalid) {
+      return;
+    }
+    const profileData = { ...this.form.value } as Profile;
+    this.userService.updateProfile(profileData).subscribe(() => {
+      this.toggleEdit();
+    });
   }
 }
